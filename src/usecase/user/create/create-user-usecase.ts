f13cef47755d37e8bcbde29d7ab26b432697d79f
Lucian Tavares <lucian.sistemas.sc@gmail.com>
2023-09-27 17:49:39 -0300

    feat: Ajusta implementação do UseCase

--- src/usecase/user/create/create-user-usecase.ts
+++ src/usecase/user/create/create-user-usecase.ts
@@ -1,34 +1,26 @@
 import {InputCreateUserDto, OutputCreateUserDto} from "./create-user-dto";
 import User from "../../../domain/entity/user";
 import {TransactionInterface} from "../../../@common/domain/transaction-interface";
+import KeycloakConnect from "keycloak-connect";
 
 export default class CreateUserUsecase {
 
-    private transaction: TransactionInterface
-
-    private userRepository: UserRepositoryInterface
-
-    constructor(userRepository: UserRepositoryInterface) {
-        this.userRepository = userRepository
+    constructor(private userRepository: UserRepositoryInterface,
+                private transaction: TransactionInterface,
+                //private identityProviderHttp: IdentityProviderHttpInterface ----> Caso queira trabalhar de forma mais abstrata poder utilizar desta forma.
+                private keycloakHttp: KeycloakConnect.Keycloak
+    ) {
     }
 
-    public setTransaction(transaction: TransactionInterface): void {
-        this.transaction = transaction
-    }
-
-    const keycloakGateway = new KeycloakGateway('https://seu-servidor-keycloak.com');
-
-    async create(input: InputCreateUserDto): Promise<OutputCreateUserDto> {
+    async execute(input: InputCreateUserDto): Promise<OutputCreateUserDto> {
 
         const user = new User(input.name, input.cpf)
 
         this.transaction.do(async (transaction) => {
-
-            await this.transaction.start(transaction)
-            await this.keycloakGateway.login('seu_usuario', 'sua_senha');
             await this.userRepository.create(user)
-            await this.transaction.commit()
-        })
+            //await this.identityProviderHttp.create(user);
+            await this.keycloakHttp.createUser(user);
+        });
 
         return user
     }
